c20b8e5b552177c2f35a241da4fafee4588fe313
windtorn <tesseract5864@duck.com>
2024-12-03 18:18:50 +0100

    Remove unnecessary debug feature

--- main.py
+++ main.py
@@ -10,8 +10,3 @@ def rsa_decrypt(input, p, q, e):
     for i in range(len(input)):
         output += chr(pow(input[i], pow(e, -1, (p - 1) * (q - 1)), p * q))
     return output
-if __name__ == "__main__":
-    # if input("Encrypt or decrypt?").lower() == "encrypt":
-    #     print(rsa_encrypt(input("\nMessage to encrypt: "), input("\nLarge prime 1: "), input("\nLarge prime 2: "), input("\nEncryption exponent: ")))
-    print(f"Encrypted: {rsa_encrypt('Hi!', 61, 53, 17)}")
-    print(f"Decrypted: {rsa_decrypt([3000, 3179, 1853], 61, 53, 17)}")
